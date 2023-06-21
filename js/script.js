b25937b05d05330f4fa261a35706ae11ed037cb3
Lamia Faria <90848499+lamiafaria19@users.noreply.github.com>
2023-06-21 17:33:44 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.count').counterUp({
+            delay: 20,
+            time: 2000
+        });
+	$('.count2').counterUp({
+            delay: 20,
+            time: 2000
+        });
+});
